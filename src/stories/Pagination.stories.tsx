ff2fa563a9dd682d0ed70133e2d303d10b1aa313
jangbi96 <di1011111@gmail.com>
2023-01-05 09:47:25 +0900

    Update Pagination.stories.tsx

--- src/stories/Pagination.stories.tsx
+++ src/stories/Pagination.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Pagination from '../components/Pagination';
 import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
 
 export default {
   title: 'components/Pagination',
@@ -9,7 +10,12 @@ export default {
   },
 } as ComponentMeta<typeof Pagination>;
 
-const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args}>버튼</Pagination>;
+const Template: ComponentStory<typeof Pagination> = (args) => {
+  let [currentPage, setCurrentPage] = useState<number>(1);
+  return <Pagination {...args}>버튼</Pagination>;
+}
+
+
 
 export const Default = Template.bind({});
 Default.args = {
@@ -17,5 +23,4 @@ Default.args = {
   listView : 10,
   pageView : 10,
   currentPage: 1
-  
 }
