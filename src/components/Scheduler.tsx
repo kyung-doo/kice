b537be644465138b25759f15c161b08dde1fdda9
kyung-doo <hohomkd@naver.com>
2023-01-06 12:36:47 +0900

    스케쥴러 추가

---
+++ src/components/Scheduler.tsx
@@ -0,0 +1,250 @@
+import { FC, HTMLProps, memo, useCallback, useEffect, useMemo, useState } from "react";
+import styled from 'styled-components';
+import Button from "./Button";
+
+
+
+
+export interface Props extends HTMLProps<HTMLDivElement> {
+   /**
+    * 데이 이름 [일, 월, 화, 수, 목, 금, 토]
+    */
+   dayNames?: string[];
+   /**
+    * 스케쥴 데이터
+    */
+   scheduleData: any[];
+   /**
+    * 스케쥴 날짜 변경
+    */
+   onChangeDate?: ( year: number, month: number ) => void;
+   /**
+    * 스케쥴 클릭 이벤트
+    */
+   onCickSchedule?: ( scheduleId: string ) => void;
+
+}
+
+/**
+ * 스케쥴러 컴포넌트
+ */
+const Scheduler: FC<Props & {as?: any}> = ({ 
+   className,
+   dayNames = ['일', '월', '화', '수', '목', '금', '토'],
+   scheduleData,
+   onChangeDate,
+   onCickSchedule,
+   as,
+   ...props
+}) => {
+
+   const [date, ] = useState<Date>(new Date());
+   const [dates, setDates] = useState<{num: number, type: string}[]>([]);
+
+   const setCalendar = ( date: Date ) => {
+      
+      const viewYear = date.getFullYear();
+      const viewMonth = date.getMonth();
+
+      const prevLast = new Date(viewYear, viewMonth, 0);
+      const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+      const PLDate = prevLast.getDate();
+      const PLDay = prevLast.getDay();
+
+      const TLDate = thisLast.getDate();
+      const TLDay = thisLast.getDay();
+
+      const prevDates = [];
+      const thisDates = [];
+      const nextDates = [];
+
+      const toDay = new Date();
+
+      for(let i=0; i<TLDate; i++) {
+         if(viewMonth === toDay.getMonth() && viewYear === toDay.getFullYear() && i + 1 === toDay.getDate()){
+            thisDates.push({num: i+1, type: 'normal today'});
+         } else {
+            thisDates.push({num: i+1, type: 'normal'});
+         }
+      }
+
+      if (PLDay !== 6) {
+         for (let i = 0; i < PLDay + 1; i++) {
+            prevDates.unshift({num: PLDate - i, type: 'prev'});
+         }
+      }
+
+      for (let i = 1; i < 7 - TLDay; i++) {
+         nextDates.push({num: i, type: 'next'})
+      }
+
+      const dates = prevDates.concat(thisDates, nextDates);
+      setDates(dates);
+   }
+
+   const onPrevMonth = useCallback(() => {
+      date.setDate(1);
+      date.setMonth(date.getMonth()-1);
+      if(onChangeDate) onChangeDate(date.getFullYear(), date.getMonth()+1);
+      setCalendar(date);
+   }, [date, dates]);
+
+   const onPrevYear = useCallback(() => {
+      date.setMonth(1);
+      date.setFullYear(date.getFullYear()-1);
+      if(onChangeDate) onChangeDate(date.getFullYear(), date.getMonth()+1);
+      setCalendar(date);
+   }, [date, dates]);
+
+   const onNextMonth = useCallback(() => {
+      date.setDate(1);
+      date.setMonth(date.getMonth()+1);
+      if(onChangeDate) onChangeDate(date.getFullYear(), date.getMonth()+1);
+      setCalendar(date);
+   }, [date, dates]);
+
+   const onNextYear = useCallback(() => {
+      date.setMonth(1);
+      date.setFullYear(date.getFullYear()+1);
+      if(onChangeDate) onChangeDate(date.getFullYear(), date.getMonth()+1);
+      setCalendar(date);
+   }, [date, dates]);
+
+   const findSchedule = useCallback(( num: number ) => {
+      const find = scheduleData.find(data => {
+         const dateStr = data.date.split("/");
+         const year = parseInt(dateStr[0]); 
+         const month = parseInt(dateStr[1]); 
+         const day = parseInt(dateStr[2]);
+         return date.getFullYear() === year && date.getMonth()+1 === month && day === num;
+      });
+      return find;
+   }, [date, scheduleData]);
+
+
+   useEffect(() => {
+      setCalendar(date);
+   }, [date]);
+
+   
+
+   return (
+      <Styled.Scheduler 
+         className={className} 
+         as={as} 
+         {...props}
+      >
+         <div className="calendar">
+            <div className="header">
+               <Button className="prevYear" onClick={onPrevYear}>{'<<'}</Button>
+               <Button className="prevMonth" onClick={onPrevMonth}>{'<'}</Button>
+               <div className="currentDate">{`${date.getFullYear()}년 ${date.getMonth()+1}월`}</div>
+               <Button className="prevMonth" onClick={onNextMonth}>{'>'}</Button>
+               <Button className="prevYear" onClick={onNextYear}>{'>>'}</Button>
+            </div>
+            <div className="day-names">
+               {Array.from(Array(7), (k, i) => (
+                  <div key={`day-name${i}`} className="day-name">{dayNames[i]}</div>
+               ))}
+            </div>
+            <div className="days">
+               {dates &&dates.map((date, i) => {
+                  const sunday = i % 7 === 0;
+                  const saturday = i % 7 === 6;
+                  return (
+                     <div 
+                        key={`day${i}`} 
+                        className={
+                           `day ${sunday && 'sunday'} ${saturday && 'saturday'} ${date.type}`
+                        }
+                     >
+                        <div className="number">{date.num}</div>
+                        {findSchedule(date.num) && 
+                           <ul className="schedule-list">
+                              {findSchedule(date.num).lists.map((list: any) => (
+                                 <li key={`schedule${list.scheduleId}`}>
+                                    <Button 
+                                       as='a' 
+                                       onClick={() => onCickSchedule && onCickSchedule(list.scheduleId)}
+                                    >
+                                       {list.title}
+                                    </Button>
+                                 </li>
+                              ))}
+                           </ul>
+                        }
+                     </div>
+                  )
+               })}
+            </div>
+         </div>
+      </Styled.Scheduler>
+   );
+}
+
+const Styled = {
+   Scheduler: styled.div<Props>`
+      .calendar{
+         border: solid 1px #ccc;
+      }
+      .header{
+         display: flex;
+         flex-direction: row;
+         align-items: center;
+         justify-content: center;
+         margin-bottom: 10px;
+         .currentDate{
+            margin: 0 10px;
+         }
+      }
+      .day-names{
+         display: flex;
+         flex-direction: row;
+         .day-name{
+            width: calc(100% / 7);
+            text-align: center;
+            border: solid 1px #ccc;
+            padding: 4px;
+            box-sizing: border-box;
+            background-color: #ffff95;
+         }
+      }
+      .days{
+         display: flex;
+         flex-direction: row;
+         flex-wrap: wrap;
+         .day{
+            width: calc(100% / 7);
+            height: 100px;
+            padding: 4px;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            border: solid 1px #ccc;
+            &.prev,
+            &.next {
+               .number {
+                  opacity: 0.3;
+               }
+            }
+            &.sunday {
+               .number{
+                  color: red;
+               }
+            }
+            &.saturday {
+               .number{
+                  color: blue;
+               }
+            }
+            &.today {
+               background-color: #bdffbd;
+            }
+         }
+      }
+      
+   `,
+}
+
+export default memo(Scheduler);
--- src/pages/Testpage.tsx
+++ src/pages/Testpage.tsx
@@ -7,6 +7,7 @@ import Select from "../components/Select";
 import RadioGroup from "../components/RadioGroup";
 import Radio from "../components/Radio";
 import Textarea from "../components/TextArea";
+import Scheduler from "../components/Scheduler";
 
 
 // select
@@ -33,6 +34,52 @@ const Testpage: FC = () => {
    
    // 텍스트박스
    const [textVal, setTextval] = useState<any>('');
+
+
+   // 스케쥴 데이터
+   const [schduleData, setScheduleData] = useState<any[]>([
+      { 
+         date: '2023/01/10', 
+         lists: [
+            {
+               scheduleId: '0',
+               date: '일시 1월 10일 (화) 15:00 ~ 16:00',
+               title: '[온라인] 충남 태안여자고등학교 (이정운, 박진)',
+               consultant: '전미정, 전종식',
+               teacher: '김지숙 선생님'
+            },
+            {
+               scheduleId: '1',
+               date: '일시 1월 10일 (화) 15:00 ~ 16:00',
+               title: '[온라인] 제주 광령초등학교 (이윤섭, 손준호)',
+               consultant: '전미정, 전종식',
+               teacher: '김지숙 선생님'
+            }
+         ]
+      },
+      { 
+         date: '2023/01/24', 
+         lists: [
+            {
+               scheduleId: '3',
+               date: '일시 1월 24일 (화) 15:00 ~ 16:00',
+               title: '[온라인] 대구 이곡중학교 (전미정, 정종식)',
+               consultant: '전미정, 전종식',
+               teacher: '김지숙 선생님'
+            }
+         ]
+      }
+   ]);
+
+   const onChangeDate = useCallback(( year: number, month: number )=> {
+      console.log('달력 변경 year: ', year, 'month: ', month);
+      // ajax 처리후 setScheduleData 변경
+   },[]);
+
+   const onCickSchedule = useCallback(( scheduleId: string ) => {
+      console.log('스케쥴 클릭: ', scheduleId);
+   },[]);
+
 
    return (
       <Styled.Testpage>
@@ -119,6 +166,14 @@ const Testpage: FC = () => {
          </RadioGroup>
 
          <Textarea placeholder={'내용을 입력해주세요.'} value={textVal} onChange={setTextval}/>
+
+         {schduleData && 
+            <Scheduler 
+               scheduleData={schduleData} 
+               onChangeDate={onChangeDate} 
+               onCickSchedule={onCickSchedule}
+            />
+         }
 
       </Styled.Testpage>
    );
